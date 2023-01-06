8fe0f3c6017a8e4e940bc1cf57ecb4ec5e89e712
Roel van Dun <roelvandun@gmail.com>
2023-01-06 14:55:00 +0100

    lengthInSeconds

--- src/resolvers.js
+++ src/resolvers.js
@@ -40,10 +40,13 @@ const resolvers = {
     author: ({ authorId }, _, { dataSources }) => {
       return dataSources.trackAPI.getAuthor(authorId);
     },
-
     modules: ({ id }, _, { dataSources }) => {
       return dataSources.trackAPI.getTrackModules(id);
     },
+    lengthInSeconds: ({ length }) => length
+  },
+  Module: {
+    lengthInSeconds: ({ length }) => length
   },
 };
 
